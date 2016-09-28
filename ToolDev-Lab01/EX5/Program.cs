6656ee8da95b48f7b23c08fbc9a6d4f8b286eae3
zecheeseking <christopher.dale.@student.howest.be>
2016-09-28 11:49:28 +0200

    Added in a bunch of the simpler exercises completed.

---
+++ ToolDev-Lab01/EX5/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EX5
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            char loop = 'y';
+
+            Console.WriteLine("Guess a number between 0 and 9");
+
+            Random rand = new Random();
+            while (loop == 'y')
+            {
+                int target = rand.Next(0, 9);
+                int guesses = 0;
+                int guess = -1;
+
+                while (target != guess)
+                {
+                    int.TryParse(Console.ReadLine(), out guess);
+                    guesses++;
+                    if (guess == target)
+                        Console.WriteLine($"Your guess: {guess} was correct in {guesses} guesses!");
+                    else
+                        Console.WriteLine($"Your guess: {guess} was incorrect!");
+                }
+
+
+
+                Console.WriteLine("Play again? Y/N");
+                loop = Console.ReadLine().ToLowerInvariant()[0];
+            }
+        }
+    }
+}
---
+++ ToolDev-Lab01/EX6/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EX6
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("First Name: ");
+            string first = Console.ReadLine();
+            Console.WriteLine("Last Name: ");
+            string last = Console.ReadLine();
+            Console.WriteLine("Birthday (no year): ");
+            string date = Console.ReadLine();
+            var splits = date.Split();
+            int day = int.Parse(splits[0]);
+            int month = int.Parse(splits[1]);
+
+            DateTime today = DateTime.Today;
+            DateTime birthday = new DateTime(today.Year, month, day);
+
+            if (birthday < today)
+                birthday.AddYears(1);
+
+            int daysLeft = (birthday - today).Days;
+
+            Console.WriteLine($"Hello {first} {last}, your birthday is in {daysLeft} days.");
+            Console.ReadLine();
+        }
+    }
+}
