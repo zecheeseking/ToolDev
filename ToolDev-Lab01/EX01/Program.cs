00dd8c22e68a4776279078f997c00ee74df52b0f
zecheeseking <christopher.dale.@student.howest.be>
2016-09-28 10:43:41 +0200

    EX1 added and complete.

---
+++ ToolDev-Lab01/EX01/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Text;
+
+namespace EX01
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello DAE!");
+            Console.ReadLine();
+        }
+    }
+}
