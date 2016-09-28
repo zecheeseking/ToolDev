6e7c9d911304a0527e7392692fa91b544d6e04d4
zecheeseking <christopher.dale.@student.howest.be>
2016-09-28 10:47:07 +0200

    Added and finished EX2

---
+++ ToolDev-Lab01/EX2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EX2
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.Write("First name: ");
+            string first = Console.ReadLine();
+            Console.Write("Last name: ");
+            string last = Console.ReadLine();
+            Console.WriteLine($"Greetings {first} {last}");
+            Console.ReadLine();
+        }
+    }
+}
