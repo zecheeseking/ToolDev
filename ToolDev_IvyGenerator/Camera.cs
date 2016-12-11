32f9b0d54bb33141ed987bc41873fcfeda3631de
zecheeseking <christopher.dale.@student.howest.be>
2016-12-11 23:54:23 +0100

    Camera rotation working with mouse, TODO: Lock it so it only works when mouse is captured in viewport.

--- ToolDev_IvyGenerator/Camera.cs
+++ ToolDev_IvyGenerator/Camera.cs
@@ -2,6 +2,8 @@
 using SharpDX.Direct3D10;
 using SharpDX.DirectInput;
 
+using System;
+
 using System.Diagnostics;
 
 namespace ToolDev_IvyGenerator
@@ -11,21 +13,24 @@ namespace ToolDev_IvyGenerator
 
         private float _camMoveSpeed = 5.0f;
 
-        private Vector3 _position;
+        private Vector3 _position = new Vector3(0.0f);
         public Vector3 Position { get { return _position;} }
-        private Quaternion _rotation;
+        private Quaternion _rotation = Quaternion.Identity;
+
+        private float _TotalYaw = 0.0f;
+        private float _TotalPitch = 0.0f;
 
         private readonly Keyboard _kb;
+        private readonly Mouse _mouse;
 
         public Matrix ProjectionMatrix { get; }
         private Matrix _viewMatrix;
         public Matrix ViewMatrix { get { return _viewMatrix; } }
 
+        private Matrix _transformationMatrix = Matrix.Identity;
+
         public Camera(float width, float height)
         {
-            _position = new Vector3(0.0f);
-            _rotation = Quaternion.Identity;
-
             ProjectionMatrix = Matrix.PerspectiveFovLH(MathUtil.PiOverFour, width / height, 0.1f, 1000f);
 
             _viewMatrix = Matrix.LookAtLH(_position + Vector3.ForwardLH, Vector3.Zero, Vector3.UnitY);
@@ -33,6 +38,8 @@ namespace ToolDev_IvyGenerator
             var di = new DirectInput();
             _kb = new Keyboard(di);
             _kb.Acquire();
+            _mouse = new Mouse(di);
+            _mouse.Acquire();
         }
 
         public void SetPosition(Vector3 position)
@@ -42,35 +49,54 @@ namespace ToolDev_IvyGenerator
 
         public void Update(float deltaT)
         {
-            _viewMatrix = Matrix.LookAtLH(_position, _position + Vector3.ForwardLH, Vector3.UnitY);
+            UpdateTransformationMatrix();
+
+            var mouseState = _mouse.GetCurrentState();
+            Vector2 mouseDelta = new Vector2(mouseState.X, mouseState.Y);
+            _TotalYaw += mouseDelta.X*5.0f*deltaT;
+            _TotalPitch += mouseDelta.Y*5.0f*deltaT;
+
+            _rotation = Quaternion.RotationYawPitchRoll((float)MathHelper.AngleToRadians(_TotalYaw), 
+                (float)MathHelper.AngleToRadians(_TotalPitch), 
+                0);
+
+            var camForward = Vector3.Transform(Vector3.ForwardLH, _rotation);
+            var camRight = Vector3.Transform(Vector3.Right, _rotation);
+            var camUp = Vector3.Cross(camForward, camRight);
+
+            _viewMatrix = Matrix.LookAtLH(_position, _position + camForward, camUp);
 
             var kbState = _kb.GetCurrentState();
-
             if (kbState.IsPressed(Key.W))
             {
                 Debug.WriteLine("Forward");
-                var forward = GetViewForward();
-                _position += forward * _camMoveSpeed * deltaT;
+                var dir = GetViewForward();
+                _position += dir * _camMoveSpeed * deltaT;
             }
             else if (kbState.IsPressed(Key.S))
             {
                 Debug.WriteLine("Backward");
-                var backward = GetViewForward() * -1;
-                _position += backward * _camMoveSpeed * deltaT;
+                var dir = GetViewForward() * -1;
+                _position += dir * _camMoveSpeed * deltaT;
             }
 
             if (kbState.IsPressed(Key.A))
             {
                 Debug.WriteLine("Left");
-                var left = GetViewRight() * -1;
-                _position += left * _camMoveSpeed * deltaT;
+                var dir = GetViewRight() * -1;
+                _position += dir * _camMoveSpeed * deltaT;
             }
             else if (kbState.IsPressed(Key.D))
             {
                 Debug.WriteLine("Right");
-                var right = GetViewRight();
-                _position += right * _camMoveSpeed * deltaT;
+                var dir = GetViewRight();
+                _position += dir * _camMoveSpeed * deltaT;
             }
+        }
+
+        private void UpdateTransformationMatrix()
+        {
+            _transformationMatrix = Matrix.Scaling(1.0f) * Matrix.RotationQuaternion(_rotation) * Matrix.Translation(_position);
         }
 
         private Vector3 GetViewForward()
---
+++ ToolDev_IvyGenerator/MathHelper.cs
@@ -0,0 +1,13 @@
+﻿
+using System;
+
+namespace ToolDev_IvyGenerator
+{
+    public class MathHelper
+    {
+        public static double AngleToRadians(float angle)
+        {
+            return (Math.PI / 180.0) * angle;
+        }
+    }
+}
